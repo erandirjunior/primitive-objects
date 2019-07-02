2829bbefd994cac76f03876c58fb28523cd5cbf5
erandir <aefs12junior@gmail.com>
2019-07-02 12:53:54 -0300

    Updated example

--- examples/index.php
+++ examples/index.php
@@ -2,4 +2,17 @@
 
 require_once '../vendor/autoload.php';
 
-$bool = \Primitive\Boolean::isBoolean(1);
+//$bool = \Primitive\Boolean::isBoolean(1);
+
+$array = new \Primitive\ArrayList();
+
+var_dump($array->getValue());
+var_dump($array->add('ok'));
+/*var_dump($array->addWithKey('chave', 'valor'));
+var_dump($array->getValue());
+var_dump($array->remove());
+var_dump($array->getValue());*/
+var_dump($array->addFirst('novo', 'novo2'));
+var_dump($array->getValue());
+var_dump($array->filter(function ($v) { if(strlen($v) == 2) {return $v;} }));
+var_dump($array->length());
